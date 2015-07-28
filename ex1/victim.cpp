28622781f59f15412177b54e4824ebd284128444
Eray <erayarslan@users.noreply.github.com>
2015-07-28 14:48:58 +0300

    Create victim.cpp

---
+++ ex1/victim.cpp
@@ -0,0 +1,15 @@
+/*
+*  author : Eray ARSLAN
+*  web    : erayarslan.com
+*/
+
+#include <iostream>
+#include <conio.h>
+ 
+int main() {
+    int data=54; // Dummy Data
+    while(0x01) {
+        std::cout<<&data<<" = "<<data<<std::endl;
+        getch();
+    } return 0x00;
+}
